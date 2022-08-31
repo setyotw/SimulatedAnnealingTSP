f44996a3434d5742dfd31484daf5f54359abd434
SETYOTW <setyotriw@gmail.com>
2022-09-01 00:15:18 +1000

    First commit

--- TSP with SA.py
+++ TSP with SA.py
@@ -1,60 +1,36 @@
+## Problem: Traveling salesman problem, objective function: minimize total distance traveled
+## Solver: Simulated annealing
+## Language: Python
+## Written by: @setyotw
+## Purpose: Public repository
+## Date: August 31, 2022
+
+#%% import packages
 import numpy as np
 import random as rand
 import pandas as pd
-#import matplotlib.pyplot as plt
 import time
 import math
 
-#%%
-# define timer
+#%%%%%%%%%%%%%%%%%%%%%%%%%%
+#  DEVELOPMENT PARTS
+#%% define timer
+# a homemade version of matlab tic and toc functions
 def tic():
-    #Homemade version of matlab tic and toc functions
     global startTime_for_tictoc
     startTime_for_tictoc = time.time()
 
 def toc():
     if 'startTime_for_tictoc' in globals():
-        print ("Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds.")
+        runtimeCount = time.time() - startTime_for_tictoc
+        print ("Elapsed time is " + str(runtimeCount) + " seconds.")
+        return runtimeCount
     else:
         print ("Toc: start time not set")
+        return int(0)
         
-#%%
-# import data
-#df = pd.DataFrame()
-df = pd.read_excel (r'F:\Work\3. Research\0. Code\4. TSP with SA - Python\instance1.xls', sheet_name='Sheet1', header=None)
-data = df.as_matrix()
-df = pd.read_excel (r'F:\Work\3. Research\0. Code\4. TSP with SA - Python\instance1.xls', sheet_name='Sheet2', header=None)
-distanceMatrix = df.as_matrix()
-df = pd.read_excel (r'F:\Work\3. Research\0. Code\4. TSP with SA - Python\instance1.xls', sheet_name='Sheet5', header=None)
-travelTimeMatrix = df.as_matrix()
-del df
-
-#%%
-numNode = len(data[:,1])
-demand = data[:,3]
-#ServiceTime = Data(:,5);
-#ServiceTime = zeros(NumNode,1);
-
-
-#%% define parameters for the heuristic
-Tmax = 150
-Tmin = 0.01
-maxNonImprove = 10000
-maxIteration = 1000
-coolingRate = 0.99
-
-#%% define parameters for the problem
-#CapacityGV = 200; % capacitated GV
-vehicleCapacity = sum(demand) #incapacitated GV
-
-#%% solution storage for the heuristic
-currentSolution = []
-X_penalty = []
-Y_penalty = []
-counter = 0
-
-#%% define some functions for the heuristic
-#%% a | function for fitness calculation
+#%% define functions needed
+#% a | fitness calculation
 def fitnessTSP(solution, distanceMatrix):
     distance = np.zeros([1,1])
     for i in range(0,len(solution[0,:])-1):
@@ -65,28 +41,32 @@ def fitnessTSP(solution, distanceMatrix):
     objective = distance
     return objective
 
-#%% b | local search function
-#% b.1 | swap '
+#% b | neighborhood moves
+# b.1 | swap move
 def swapSearch(solution):
-    swapNodes = rand.sample(range(1,len(X_sol[0,:])-1),2)
+    # select two different nodes
+    swapNodes = rand.sample(range(1,len(solution[0,:])-1),2)
     swap1 = swapNodes[0]
     swap2 = swapNodes[1]
     
+    # exchange the position of those nodes
     new_solution = np.array(solution)
     temp2 = np.array(new_solution[0][swap1])
     new_solution[0][swap1] = new_solution[0][swap2]
     new_solution[0][swap2] = temp2
-    del (swap1,swap2,temp2,swapNodes)
+    
     return new_solution
 
-#% b.2 | insertion
+# b.2 | insertion move
 def insertionSearch(solution):
-    insertionNodes = rand.sample(range(1,len(X_sol[0,:])-1),2)
+    # select two different nodes
+    insertionNodes = rand.sample(range(1,len(solution[0,:])-1),2)
     while abs(insertionNodes[0]-insertionNodes[1]) == 1 or abs(insertionNodes[0]-insertionNodes[1]) == 0:
-        insertionNodes = rand.sample(range(1,len(X_sol[0,:])-1),2)
+        insertionNodes = rand.sample(range(1,len(solution[0,:])-1),2)
     insert1 = insertionNodes[0]
     insert2 = insertionNodes[1] # target
     
+    # move one of the node behind the other one
     new_solution = np.array(solution)
     if insert1<insert2:
         temp2 = np.array(new_solution)
@@ -102,22 +82,23 @@ def insertionSearch(solution):
             new_solution[0][insert2] = np.array(temp2[0][insert2+1])
         else:
             new_solution[0][insert2:insert1-1] = np.array(temp2[0][insert2+1:insert1])
-    del (insertionNodes,insert1,insert2)
+    
     return new_solution
 
-#% b.3 | 2-opt (double bridge)
+# b.3 | 2-opt (double bridge) move
 def twoOptSearch(solution):
+    # select four different nodes
     new_solution = np.array(solution)
-    edgeNodes = rand.sample(range(1,len(X_sol[0,:])-2),2)
+    edgeNodes = rand.sample(range(1,len(solution[0,:])-2),2)
     while abs(edgeNodes[0]-edgeNodes[1]) == 1:
-        edgeNodes = rand.sample(range(1,len(X_sol[0,:])-2),2)
+        edgeNodes = rand.sample(range(1,len(solution[0,:])-2),2)
     np.sort(edgeNodes)    
     edgeA = edgeNodes[0]
     edgeB = edgeNodes[0]+1
     edgeC = edgeNodes[1]
     edgeD = edgeNodes[1]+1
     
-    #swap them with 2-opt rule
+    # swap them with 2-opt rule
     nodeA = new_solution[0][edgeA]
     nodeB = new_solution[0][edgeC]
     nodeC = new_solution[0][edgeB]
@@ -126,76 +107,109 @@ def twoOptSearch(solution):
     new_solution[0][edgeB] = nodeB
     new_solution[0][edgeC] = nodeC
     new_solution[0][edgeD] = nodeD
-    del (edgeA, edgeB, edgeC, edgeD, nodeA, nodeB, nodeC, nodeD)
+    
     return new_solution
 
+# c | simulated annealing 
+def simulatedAnnealing(distanceMatrix, numNodes, temperatureMax, temperatureMin, maxNonImprove, maxIteration, coolingRate):
 
-#%%
+    # 1 | initialize a solution storage
+    solutionRepo = []
 
-#%%
-#% simulated annealing main algorithm    
-tic()
+    # 2 | initialize a solution storage, current temperature, and iterationCount counters
+    temperatureNow = temperatureMax
+    iterationCount = int(1)
+    nonImprovementCounter = int(0)
 
-#% 1 | initialize temperature and iteration
-temperature = Tmax
-iteration = 1
-nonImprove = 0
+    # 3 | create an initial TSP solution with random solution
+    currentSol = np.zeros([1,numNodes+1], dtype = int)
+    currentSol[0,1:-1] = np.random.permutation(numNodes-1)+1
 
-#% 2 | create initial solution with random solution
-X_sol = np.zeros([1,numNode+1], dtype = int)
-X_sol[0,1:-1] = np.random.permutation(numNode-1)+1
-
-#% 3 | evaluate the solution
-X_objective = fitnessTSP(X_sol, distanceMatrix)
-
-#% 4 | Main iteration of the simulated annealing
-while temperature >= Tmin:
+    # 4 | evaluate the solution and store it as the current & best solution
+    currentObjective = fitnessTSP(currentSol, distanceMatrix)
+    bestObjective = np.array(currentObjective)
+    bestSol = np.array(currentSol)
     
-#%
-#% 5 | create new solution based on a random number and the current solution
-    r = np.random.random()
+    # 5 | Main iteration of the algorithm
+    tic()
+    while temperatureNow >= temperatureMin:
+
+        # 5.1 | create a new TSP solution based using a selected neighborhood move
+        randomNum = np.random.random()
+        if randomNum <= 0.33:
+            newSol = swapSearch(currentSol)
+        elif (randomNum > 0.33) and (randomNum <= 0.66):
+            newSol = insertionSearch(currentSol)
+        else:
+            newSol = twoOptSearch(currentSol)
+
+        # 5.2 | evaluate the new TSP solution
+        newObjective = fitnessTSP(newSol, distanceMatrix)
+
+        # 5.3 | check if newSol is better than currentSol (note that we are working with a minimization problem)
+        deltaObjective = newObjective - currentObjective
+        if deltaObjective < 0:
+            currentSol = np.array(newSol)
+            currentObjective = np.array(newObjective)
+            if currentObjective < bestObjective:
+                bestObjective = np.array(currentObjective)
+                bestSol = np.array(currentSol)
+            nonImprovementCounter = int(0)
+        else:
+            randomNum = np.random.random()
+            if randomNum < math.exp(-deltaObjective/temperatureNow):
+                currentObjective = np.array(newObjective)
+                currentSol = np.array(newSol)
+            nonImprovementCounter += 1
+
+        # 5.4 | storing the current solution
+        solutionRepo.append(bestObjective)
+
+        # 5.5 | checking the termination conditions and update the iterationCount    
+        if nonImprovementCounter >= maxNonImprove:
+            break
+        else:
+            iterationCount = iterationCount+1
+            if iterationCount == maxIteration:
+                temperatureNow = temperatureNow*coolingRate
+                iterationCount = int(1)
+    runtimeCount = toc()
     
-    if r <= 0.33:
-        Y_sol = swapSearch(X_sol)
-    elif (r > 0.33) and (r <= 0.66):
-        Y_sol = insertionSearch(X_sol)
-    else:
-        Y_sol = twoOptSearch(X_sol)
-    del r
-   #%
-#% 6 | evaluate the new solution
-    Y_objective = fitnessTSP(Y_sol, distanceMatrix)
+    return bestSol, bestObjective, solutionRepo, runtimeCount
 
-#%
-#% 7 | check if Y_sol is better than X_sol (minimization)
-    delta = Y_objective - X_objective
-    if delta < 0:
-        X_sol = np.array(Y_sol)
-        X_objective = np.array(Y_objective)
-        nonImprove = 0
-    else:
-        nonImprove += 1
-        r = np.random.random()
-        if r < math.exp(-delta/temperature):
-            X_sol = np.array(Y_sol)
-            X_objective = np.array(Y_objective)
-        del r
+#%%%%%%%%%%%%%%%%%%%%%%%%%%
+#  IMPLEMENTATION PARTS
+#%% input problem instance
+# a simple TSP case with 1 depot and 10 customer nodes
 
-#%
-#% 8 | storing the current solution
-    currentSolution.append(X_objective)
+# symmetric distance matrix [11 x 11]
+distanceMatrix = np.array([
+[0.000, 2.768, 7.525, 9.689, 28.045, 36.075, 25.754, 3.713, 2.701, 8.286, 7.944],
+[2.768, 0.000, 9.164, 11.482, 27.779, 37.431, 25.488, 7.305, 1.928, 7.911, 10.758],
+[7.525, 9.164, 0.000, 16.297, 33.406, 42.246, 31.115, 4.065, 8.062, 13.970, 1.594],
+[9.689, 11.482, 16.297, 0.000, 19.053, 27.198, 16.762, 12.484, 8.810, 17.182, 20.464],
+[28.045, 27.779, 33.406, 19.053, 0.000, 10.138, 5.596, 31.349, 26.286, 36.994, 38.820],
+[36.075, 37.431, 42.246, 27.198, 10.138, 0.000, 10.634, 38.434, 34.759, 43.132, 46.850],
+[25.754, 25.488, 31.115, 16.762, 5.596, 10.634, 0.000, 29.058, 23.995, 32.498, 36.529],
+[3.713, 7.305, 4.065, 12.484, 31.349, 38.434, 29.058, 0.000, 6.005, 8.615, 5.651],
+[2.701, 1.928, 8.062, 8.810, 26.286, 34.759, 23.995, 6.005, 0.000, 9.862, 9.656],
+[8.286, 7.911, 13.970, 17.182, 36.994, 43.132, 32.498, 8.615, 9.862, 0.000, 15.313],
+[7.944, 10.758, 1.594, 20.464, 38.820, 46.850, 36.529, 5.651, 9.656, 15.313, 0.000]])
 
-#% 9 | checking and update the iteration    
-    iteration = iteration+1
-    if iteration == maxIteration:
-        temperature = temperature*coolingRate
-        iteration = 1
-        
-    if nonImprove >= maxNonImprove:
-        break
-    
-    #print(iteration)
-#%
-toc()
+# number of nodes on the graph, can be calculated as the horizontal/vertical length of the distance matrix
+numNodes = len(distanceMatrix[0:])
 
-print(X_objective)
+#%% define parameters for the simulated annealing
+# note that all of these numbers are selected arbitrarily
+temperatureMax = int(150) # starting temperature
+temperatureMin = float(0.01) # lower bound temperature
+maxNonImprove = int(10000) # maximum number of iterations with non-improvement on the best solution found, to terminate
+maxIteration = int(1000) # maximum iteration per temperature
+coolingRate = int(0.99) # cooling rate of temperature
+
+#%% implement the simulated annealing to solve TSP
+# bestSol --> the TSP solution with best objective value
+# bestObjective --> best objective value found
+# solutionRepo --> a repository of best solutions found on each iteration (to see the convergence of the algorithm)
+# runtimeCount --> return the runtime of the algorithm in seconds
+bestSol, bestObjective, solutionRepo, runtimeCount = simulatedAnnealing(distanceMatrix, numNodes, temperatureMax, temperatureMin, maxNonImprove, maxIteration, coolingRate)
